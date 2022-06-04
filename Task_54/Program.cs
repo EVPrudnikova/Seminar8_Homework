8f3950a5e4744280aac272ca9ee89688c04ce40c
ElenaPrudnikova <elena8sen@yandex.ru>
2022-06-04 15:31:00 +0300

    add Task_54

---
+++ Task_54/Program.cs
@@ -0,0 +1,73 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит 
+по убыванию элементы каждой строки двумерного массива. */
+
+int m = Input("m");
+int n = Input("n");
+
+int[,] array = new int[m, n];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    int[] tempArray = new int[array.GetLength(1)];
+    for(int j = 0; j < array.GetLength(1); j++)
+    {
+        tempArray[j] = array[i, j];
+    }
+    tempArray = SortArray(tempArray);
+
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = tempArray[j];
+    }
+}
+
+Console.WriteLine("Сортированный массив: ");
+PrintArray(array);
+
+int[] SortArray(int[] array)
+{
+	for (int i = 0; i < array.Length; i++)
+    {
+        for (int j = 0; j < array.Length - 1; j++)
+        {
+            if (array[j] < array[j + 1])
+	        {
+		        int temp = array[j + 1];
+		        array[j + 1] = array[j];
+		        array[j] = temp;
+    	    }
+        }
+    }
+	return array;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int rows = 0; rows < matrix.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matrix.GetLength(1); columns++)
+        {
+            Console.Write($"{matrix[rows, columns]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(int[,] matrix)
+{
+    for (int rows = 0; rows < matrix.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matrix.GetLength(1); columns++)
+        {
+            matrix[rows, columns] = new Random().Next(1, 11);
+        }
+    }
+}
+int Input(string numberName)
+{
+    Console.Write($"Введите значение для {numberName}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
